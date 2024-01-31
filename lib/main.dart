15450c3ad40896277fd0f6eee55953878832dee2
soham603 <111444627+soham603@users.noreply.github.com>
2024-01-31 23:02:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const StartScreen(),
+      home:  const AuthPage(),
     );
   }
 }
